956c3795bab4f01f79d08f9e7385658c2008a59c
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-22 23:13:05 +0300

    bg check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,18 @@ export default function Home() {
   let id:any;
   let userName:any;
   let url:any;
+  let bg:any
   useEffect(() => {
     id = window?.Telegram?.WebApp.initDataUnsafe.user?.id
     userName = window?.Telegram?.WebApp.initDataUnsafe.user?.username
     url = window?.Telegram?.WebApp.initDataUnsafe.user?.photo_url
+    bg = window.Telegram.WebApp.backgroundColor
   }, [])
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col gap-6 items-center " >
+        <p>{bg} check</p>
         {url &&
           <Image src={url} alt='avatar' width={300} height={300} />
         } 
