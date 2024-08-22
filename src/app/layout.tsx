cbbb02964e1efc261718e6bec7eed3b5923e86f8
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-22 22:22:51 +0300

    testing web-app api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script src="https://telegram.org/js/telegram-web-app.js"></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
