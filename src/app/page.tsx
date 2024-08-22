f26668e79e7c4f70507e30838dd6e4f4056d2105
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-22 22:45:46 +0300

    fix error window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client"
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  const id = window.Telegram?.WebApp.initDataUnsafe.user?.id
-  const userName = window.Telegram?.WebApp.initDataUnsafe.user?.username
-  const url = window.Telegram?.WebApp.initDataUnsafe.user?.photo_url
+  let id:any;
+  let userName:any;
+  let url:any;
+  useEffect(() => {
+    id = window?.Telegram?.WebApp.initDataUnsafe.user?.id
+    userName = window?.Telegram?.WebApp.initDataUnsafe.user?.username
+    url = window?.Telegram?.WebApp.initDataUnsafe.user?.photo_url
+  }, [])
+ 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col gap-6 items-center " >
