fc63ec8951132f0f53e21fd736d175b353d6120f
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-23 23:50:45 +0300

    edit env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,6 @@ export default function Home() {
 
   },[])
 
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col gap-6 items-center " >
