f50fe0ef4a3da7d9f71febffdf1263bf425d8f49
Artur <nevisipaus@gmail.com>
2024-08-25 02:10:47 +0300

    get lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
 "use client"
 import { initializeUser, Button } from "@/shared";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { DrawerWrapper } from "@/entities";
 import { CreateListForm } from "@/features";
+import { getLists } from "@/shared/lib/get-lists";
 
 export default function Home() {
   const [id, setId] = useState<number | undefined>(undefined);
   const [userName, setUserName] = useState<string | undefined>(undefined);
 
+  const user = window.Telegram.WebApp.initDataUnsafe.user
   useEffect(() => {
-    const user = window.Telegram.WebApp.initDataUnsafe.user
+    
     setId(user?.id)
     setUserName(user?.first_name)
 
-    if(user?.id) initializeUser(`${user?.id}`, user?.first_name)
+    if(user?.id) {
+      initializeUser(`${user?.id}`, user?.first_name)
+    }
   },[])
+
+  if(user?.id) {
+    const lists = getLists(`${user?.id}`) 
+    console.log(lists)
+  }
 
   return (
     <main className="p-6 flex flex-col gap-6 items-center ">
---
+++ src/shared/lib/get-lists.ts
@@ -0,0 +1,10 @@
+"use server"
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../api";
+
+export const getLists = (id:string) => {
+    const unsub = onSnapshot(doc(db, `users/${id}/lists`), (doc) => {
+        console.log("Current data: ", doc.data());
+        return doc.data()
+    });
+}
