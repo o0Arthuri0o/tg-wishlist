78ab4bdd8d5f56a4436e971757158999eb10c873
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-27 11:07:22 +0300

    get list info

--- src/app/[list-id]/page.tsx
+++ src/app/[list-id]/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 
 export default async function ListPage({params}:{params: {'list-id': string}}) {
-  const id = params["list-id"].slice(3).slice(0, 21)
-  // const list = await GetList(id)
+  const id = params["list-id"].slice(3).slice(0, 20)
+  const list = await GetList(id)
   return (
     <div>
       <Link href={'/'} >back</Link>
       <div>page {id} </div>
-      {/* <div>{list?.title}</div> */}
+      <div>{list?.title}</div>
     </div>
     
   )
