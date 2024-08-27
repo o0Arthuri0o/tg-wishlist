e16b1f269057641c1735c4bbf1e2a01a799cb5b2
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-27 21:34:19 +0300

    check id

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,13 +2,13 @@ import { getList } from "@/shared";
 import Link from "next/link";
 
 
-export default async function ListPage({params}:{params: {'list-id': string}}) {
-  const id = params["list-id"].slice(3).slice(0, 20)
-  const list = await getList(id)
+export default async function ListPage({params}:{params: {id: string}}) {
+  const id = params.id
+  // const list = await getList(id)
   return (
     <div className="p-6 " >
       <Link href={'/'} >back</Link>
-      <p className="text-base " >{id} - {list?.title}</p>
+      <p className="text-base " >{id}</p>
       <div className="mt-6  " >
         список подарков
       </div>
