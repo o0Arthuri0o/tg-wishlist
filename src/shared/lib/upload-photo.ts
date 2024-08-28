fa9851b21241be505ac4e46788c0fca46857297d
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-28 13:53:57 +0300

    check upload

--- src/shared/lib/upload-photo.ts
+++ src/shared/lib/upload-photo.ts
@@ -5,6 +5,7 @@ import { storage } from "../api";
 export const uploadPhoto = (idGift:string, gift:FormData ) => {
     const giftObj = Object.fromEntries(gift)
     const file = giftObj.photo as File 
+    console.log(file, file.name, 'file check')
     const storageRef = ref(storage, `${idGift}/${file.name}`);
 
     uploadBytes(storageRef, file).then((snapshot) => {
