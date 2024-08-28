4ad87ae96a3b10aef9ffb1f4fa492f85b318bd14
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-08-28 23:27:19 +0300

    edit

--- src/entities/gift-card/ui/gift-card.tsx
+++ src/entities/gift-card/ui/gift-card.tsx
@@ -1,17 +1,17 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/shared";
+import { Button, Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/shared";
 import { Gift } from "@/shared/lib/get-gifts";
-import { Gift as GiftSVG } from "lucide-react";
+import { Gift as GiftSVG, Pencil, Trash2 } from "lucide-react";
 import Link from "next/link";
 
 export function GiftCard({gift}:{gift:Gift}) {
   return (
     <Card className="p-2 w-full h-auto flex items-center gap-2" >
-        <div className="w-[70px] h-[70px] bg-slate-200 rounded-xl flex justify-center items-center " >
+        <div className="min-w-[70px] h-[70px] bg-slate-200 rounded-xl flex justify-center items-center " >
             <GiftSVG width={40}  height={40} />
         </div>
-        <div className="flex flex-col gap-3 " >
+        <div className="flex flex-col gap-3 justify-between" >
             <CardHeader>
-                <CardTitle>
+                <CardTitle className="p-0">
                     {gift.name}
                 </CardTitle>
                 {gift.description.length > 0 &&
@@ -20,11 +20,21 @@ export function GiftCard({gift}:{gift:Gift}) {
                     </CardDescription>
                 }
             </CardHeader>
-            <CardContent className="flex justify-between gap-3" >
-                {gift.link.length > 0 &&
-                    <Link href={gift.link} className="p-2 text-blue-400" >В магазин</Link>
-                }
-                <p className="text-base p-2" >{gift.price}</p>
+            <CardContent className="flex flex-col gap-2 p-0" >
+                <div className="flex justify-between gap-3 p-0" >
+                    {gift.link.length > 0 &&
+                        <Link href={gift.link} className="p-2 text-blue-400" >В магазин</Link>
+                    }
+                    <p className="text-base p-2" >{gift.price}</p>
+                </div>
+                <div className="flex gap-2 items-center justify-between " >
+                    <Button variant={'secondary'} >
+                        <Pencil/>
+                    </Button>
+                    <Button variant={'destructive'} >
+                        <Trash2/>
+                    </Button>
+                </div>
             </CardContent>
         </div>
     </Card>
